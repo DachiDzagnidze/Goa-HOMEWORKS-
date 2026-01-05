f8db2f1047579c83b3a994353bc7f47421c84db2
bgharashvilimarii <mariambgharashvili@gmail.com>
2026-01-05 23:06:14 +0400

    second commit

---
+++ day025/classwork/main.py
@@ -0,0 +1,19 @@
+# names = ["mari", "lika", "dachi", "giorgi", "natia"]
+# print(names)
+# print(names[2])
+# print(names[3])
+# names[4] = "mari"
+# print(names)
+# names[1],names[2] = names[2],names[1]
+# print(names)
+
+
+
+# names = ["mari", "lika", "dachi", "giorgi", "natia"]
+# print(names[-2])
+
+my_list = ["mari", 5 , 5.5 , True , False, "wyali", 19]
+print(len(my_list))
+
+name = input("enter your name: ")
+print(len(name))
---
+++ day025/homework/main.py
@@ -0,0 +1,18 @@
+# indexsi aris elementis unikaluri ricxvi romelic miutitebs mis pozicias pythonshi indexsebis atvla iwkeba 0idan 
+# aseve gvaqvs uarkofiTi indexsebi romlebis pirikit uknidan moukveba sias (bolo elementidan pirvelisaken) da atvla am shemtxvevashi iwkeba -1idan.
+
+
+
+slay = [1, "okey", "dachi", 23.5, "lobiani"]
+print(slay)
+print(slay[3])
+print(slay[4])
+slay[2] = "mari"
+print(slay)
+slay[0],slay[3] = slay[3],slay[0]
+print(slay)
+
+
+names = ["nika", "luka", "mari", "lika", "dachi"]
+print(names[-3])
+print(len(names))
